9dc36550280756b9e07e780adcce38e0672b1809
QequeLolle <super12max-hope12@yandex.ru>
2025-10-23 11:53:02 +0300

    initial commit for lab dbHibernate

---
+++ src/main/java/dbHibernate/dao/CourseDaoConfig.java
@@ -0,0 +1,45 @@
+package dbHibernate.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("jdbc.properties")
+@ComponentScan("lab4_2.dao")
+public class CourseDaoConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource webDataSource() {
+
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName(env.getRequiredProperty("jdbc.driverClassName"));
+        ds.setUrl(env.getRequiredProperty("jdbc.url"));
+        ds.setUsername(env.getRequiredProperty("jdbc.username"));
+        ds.setPassword(env.getRequiredProperty("jdbc.password"));
+
+        return ds;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(webDataSource());
+    }
+
+//    @Bean
+
+
+
+
+
+}
